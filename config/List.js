fbc3fe2a09ac6a2b9090573dde2245992449f1b4
alexP-bu <apozin@bu.edu>
2023-05-07 06:58:17 +0000

    Updated project

---
+++ config/List.js
@@ -0,0 +1,9 @@
+class List {
+  constructor(name, updated, books){
+      this.name = name;
+      this.updated = updated;
+      this.books = books;
+  }
+}
+
+module.exports = List;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const axios = require('axios');
 const API = require('./config/API');
 const Book = require('./config/Book');
+const List = require('./config/List')
 
 const app = express();
 
@@ -12,23 +13,27 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
-    const urls = [API.URL_HARDCOVER_FICTION, API.URL_HARDCOVER_NONFICTION];
-    const [fic, non] = await Promise.all(urls.map(getBooks));
-    res.render('layout', {results: {fic: fic, non: non}});
+	const results = await getBooks2();
+	res.render('layout', { results: results });
 });
 
-const getBooks = async url  => {
-    try {
-      const res = await axios.get(url + "?api-key=" + API.Key);
-      return res.data.results.books.map(book => new Book(              
-        book.primary_isbn13,
-        book.title,
-        book.author,
-        'https://www.bookshop.org/a/15886/' + book.primary_isbn13
-      ));
-    } catch {
-      console.log(err => console.log(err));
-    }
-};
+async function getBooks2() {
+	try {
+		const res = await axios.get(API.URL_FULL_OVERVIEW + "?api-key=" + API.Key);
+		console.log("GOT RESULT")
+		return res.data.results.lists.map(list => new List(
+		  	list.list_name,
+			list.updated,
+			list.books.map(book => new Book(
+				book.primary_isbn13,
+				book.title,
+				book.author,
+				'https://www.bookshop.org/a/15886/' + book.primary_isbn13
+			))
+		))
+	} catch {
+		  console.log(err => console.log(err));
+	}
+}
 
 app.listen(8080);
